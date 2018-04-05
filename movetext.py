982a456d1daf466a7729def38875af3a128420c9
Jacky Lau <hoheilau@gmail.com>
2018-04-05 20:14:14 +0800

    Pyglet and OpenGL, tidy up line calling func.

--- movetext.py
+++ movetext.py
@@ -10,7 +10,7 @@ def map_range(OldValue, OldMin, OldMax, NewMin, NewMax):
 
 class MoveText(text.Label):
 
-    def __init__(self, text='', font_name=None, font_size=None, bold=False, italic=False, color=None, x=0, y=0, width=5, height=None, anchor_x='left', anchor_y='baseline', align='left', multiline=True, dpi=None, batch=None, group=None):
+    def __init__(self, text='', font_name=None, font_size=None, bold=False, italic=False, color=None, x=0, y=0, width=5, height=None, anchor_x='left', anchor_y='top', align='left', multiline=True, dpi=None, batch=None, group=None):
         super().__init__(text, font_name, font_size, bold, italic, color, x, y,
                          width, height, anchor_x, anchor_y, align, multiline, dpi, batch, group)
         self.alpha = 1
--- pyglet_demo.py
+++ pyglet_demo.py
@@ -1,73 +1,81 @@
 import pyglet
-from pyglet import text, font
+from pyglet import text, font, graphics
 from pyglet.window import mouse
 from time import time
 from movetext import MoveText
 from random import randrange
 import emoji
+import csv
 
-font.add_file('font\DFHsiuW3.ttc')
 
-def loadLines(filepath):
-    # import csv
-    # with open('fbContent.txt', 'r', encoding='utf-8') as fp:
-    #     scr = []
-    #     for line in fp.read().split('\n'):
-    #         scr.append(line)
-    #     return scr
-    with open('fbContent.txt', 'r', encoding='utf-8') as fp:
-        scr = []
+def loadLinesFromTxt(filepath):
+    scr = []
+    with open(str(filepath), 'r', encoding='utf-8') as fp:
         for line in fp.read().split('\n'):
-            # if any(char in emoji.UNICODE_EMOJI for char in line):
             for char in line:
                 if char in emoji.UNICODE_EMOJI:
-                    line = line.replace(char,'')
+                    line = line.replace(char, '')
             scr.append(line)
-        return scr
-
-# def loadLines(filepath):
-#     import csv
-#     fp = open(str(filepath), 'r', encoding='utf-8')
-#     scr = []
-#     for row in csv.DictReader(fp):
-#         if (float(row['polarity']) < -0.2):
-#             # print("{0:.02f}".format(
-#             #     float(row['polarity'])), '|', row['string'])
-#             scr.append(row['string'])
-#     fp.close()
-#     return scr
-
-textlist=[]
-
-def createLine():
-    y = randrange(0, window.height)
-    for t in textlist:
-        while abs(y-t.y) < 20:
-            y = randrange(0, window.height)
-
-    return MoveText(text=textlines[randrange(0, len(textlines))], font_size=30, x=randrange(
-        0, window.width/3), y=y, color=(255, 255, 255, 255),font_name='DFPHsiuW3-B5')
+    return scr
 
 
-createTime = time()
+def loadLinesFromCsv(filepath):
+    scr = []
+    with open(str(filepath), 'r', encoding='utf-8') as fp:
+        for row in csv.DictReader(fp):
+            if (float(row['polarity']) < -0.2):
+                # print("{0:.02f}".format(
+                #     float(row['polarity'])), '|', row['string'])
+                scr.append(row['string'])
+    return scr
+
+
+font.add_file(r'font\DFHsiuW3.ttc')
+font.add_file(r'font\AdobeFanHeitiStdB.otf')
+font.add_file(r'font\NotoSansCJKtc-Bold.otf')
+font.add_file(r'font\NotoSansCJKtc-Black.otf')
 
 window = pyglet.window.Window(width=1920, height=640)
 fps_display = pyglet.window.FPSDisplay(window)
+batch_text = graphics.Batch()
+# textlines = loadLinesFromCsv("NegPolarity-TextBlob-Default.csv")
+textlines = loadLinesFromTxt("fbContent.txt")
 
-textlines = loadLines("NegPolarity-TextBlob-Default.csv")
 
-# label = MoveText(text='Hello World', font_name='Times New Roman', font_size=100,
-#                  x=window.width//2, y=window.height//2, anchor_x='center', anchor_y='center', color=(50, 0, 125, 255))
+def createLine(textlist):
+    y = randrange(0, window.height)
+    for t in textlist:
+        # while any(abs(y-t.y) < 20 for t in textlist):
+        while abs(y-t.y) < 20:
+            y = randrange(0, window.height)
 
-textlist = [createLine()]
+    # return MoveText(text=textlines[randrange(0, len(textlines))], font_size=30, x=randrange(0, window.width/3), y=y, color=(255, 255, 255, 255),font_name='DFPHsiuW3-B5')
+    # return MoveText(text=textlines[randrange(0, len(textlines))], font_size=20, x=randrange(0, window.width/3), y=y, color=(255, 255, 255, 255),font_name='Adobe Fan Heiti Std B')
+    return MoveText(text=textlines[randrange(0, len(textlines))], font_size=20, x=randrange(0, window.width/3), y=y, color=(255, 255, 255, 255), font_name='Noto Sans CJK TC Bold', batch=batch_text)
+    # return MoveText(text=textlines[randrange(0, len(textlines))], font_size=20, x=randrange(0, window.width/3), y=y, color=(255, 255, 255, 255),font_name='Noto Sans CJK TC Black')
+
+
+textlist = list()
+textlist.append(createLine(textlist))
+createTime = time()
+
+
+def create_quad_vertex_list(x, y, width, height):
+    return x, y, x + width, y, x + width, y + height, x, y + height
+
+
+quad = pyglet.graphics.vertex_list(4,
+                                   ('v2i', create_quad_vertex_list(0, 0, 1920, 640)),
+                                   ('c3B', (0, 0, 0, 205, 205, 205, 205, 205, 205, 0, 0, 0)))
 
 
 @window.event
 def on_draw():
     window.clear()
-    # label.draw()
-    for t in textlist:
-        t.draw()
+    # for t in textlist:
+    #     t.draw()
+    quad.draw(pyglet.gl.GL_QUADS)
+    batch_text.draw()
     fps_display.draw()
 
 
@@ -75,13 +83,14 @@ def on_draw():
 def update(dt):
     global createTime
     if time() - createTime > randrange(3, 6):
-        textlist.append(createLine())
+        textlist.append(createLine(textlist))
         createTime = time()
     for i, t in enumerate(textlist):
         if not t.end:
             t.update()
         else:
             textlist.pop(i)
+    # print(len(textlist))
 
 
 pyglet.clock.schedule_interval(update, 1/120.0)
